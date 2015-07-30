7151ec659980c436a1174bf78cf2290818121b61
laxminarsimhak <laxminarsimha.kongari@verizon.com>
2015-07-30 06:29:46 -0700

    Create SPMIDs.java

---
+++ src/main/java/com/timesheet/SPMIDs.java
@@ -0,0 +1,23 @@
+package main.java.com.timesheet;
+
+public class SPMIDs {
+	private String spmId;
+	private String description;
+
+	public String getSpmId() {
+		return spmId;
+	}
+
+	public void setSpmId(String spmId) {
+		this.spmId = spmId;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
