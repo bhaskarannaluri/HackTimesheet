133624c804a2b598f4b3cd5e3b5572fbe355afa4
laxminarsimhak <laxminarsimha.kongari@verizon.com>
2015-07-30 07:13:19 -0700

    commit

--- src/main/java/com/timesheet/EmpSpmMapBean.java
+++ src/main/java/com/timesheet/EmpSpmMapBean.java
@@ -14,11 +14,29 @@ import java.util.Map;
 
 @ManagedBean
 @SessionScoped
-
 public class EmpSpmMapBean {
 	public List<EmpSpmMap> empSpmMapList;
 
 	public List<EmpSpmMap> getEmpSpmMapList() {
+		empSpmMapList = new ArrayList<EmpSpmMap>();
+		EmpSpmMap map = new EmpSpmMap();
+		map.emp=new Employee();
+		map.spmIds= new SPMIDs();
+		map.emp.setEmpName("Bhaskar");
+		map.spmIds.setSpmId("11060A1");
+		map.setFromDate("30-Jul-2015");
+		map.setToDate("5-Aug-2015");
+		map.setHrs(40);
+		empSpmMapList.add(map);
+		EmpSpmMap map1 = new EmpSpmMap();
+		map1.emp=new Employee();
+		map1.spmIds= new SPMIDs();
+		map1.emp.setEmpName("Hima");
+		map1.spmIds.setSpmId("11060A2");
+		map1.setFromDate("30-Jul-2015");
+		map1.setToDate("5-Aug-2015");
+		map1.setHrs(40);
+		empSpmMapList.add(map1);
 		return empSpmMapList;
 	}
 
